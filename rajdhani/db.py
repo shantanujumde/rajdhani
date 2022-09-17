33c15b6b8466471f25ba92eca33725d93c1a403e
shantanujumde <shantanu.jumde@travelopia.com>
2022-09-17 15:05:19 +0530

    task2

--- rajdhani/db.py
+++ rajdhani/db.py
@@ -17,12 +17,15 @@ def search_stations(q):
     The q is the few characters of the station name or
     code entered by the user.
     """
-    res = (db_ops.exec_query(f"select * from train where from_station_code = '{q}';"))
+    res = db_ops.exec_query(f"select * from train where from_station_code like '%{q}%';")
     # TODO: make a db query to get the matching stations
     # and replace the following dummy implementation
-    print(res)
-    return {}
-
+    ans = []
+    for val in res:
+        d = {"code":val[4], "name":val[1]}
+        ans.append(d)
+    return ans
+# {"code": "ADI", "name": "AHMEDABAD JN"},
 def search_trains(
         from_station_code,
         to_station_code,
