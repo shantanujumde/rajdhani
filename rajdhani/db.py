6bb3a589a4c1e626aac66ed6f57b9476d717365a
shantanujumde <shantanu.jumde@travelopia.com>
2022-09-17 17:40:33 +0530

    4

--- rajdhani/db.py
+++ rajdhani/db.py
@@ -29,6 +29,8 @@ def search_stations(q):
     ans = []
     return ans
 # {"code": "ADI", "name": "AHMEDABAD JN"},
+from datetime import datetime
+
 def search_trains(
         from_station_code,
         to_station_code,
@@ -42,6 +44,12 @@ def search_trains(
 
     This is used to get show the trains on the search results page.
     """
+    slot1 = datetime.strptime('00:00:00', '%H:%M:%S')
+    slot2 = datetime.strptime('08:00:00', '%H:%M:%S')
+    slot3 = datetime.strptime('12:00:00', '%H:%M:%S')
+    slot4 = datetime.strptime('16:00:00', '%H:%M:%S')
+    slot5 = datetime.strptime('20:00:00', '%H:%M:%S')
+
     # TODO: make a db query to get the matching trains
     # and replace the following dummy implementation
     col, rows = db_ops.exec_query(f"select * from train \
@@ -50,7 +58,7 @@ def search_trains(
         to_station_code like '%{to_station_code}%'\
                 ;")
     print(ticket_class, col)
-    ans = []
+    filter1 = []
     for val in rows:
         # print(val)
         d = {
@@ -66,17 +74,42 @@ def search_trains(
             "duration_m": val[11]
         }
         if ticket_class == "SL" and val[-6] == 1:
-            print("y")
-            ans.append(d)
+            filter1.append(d)
         elif ticket_class == "CC" and val[-1] == 1:
-            ans.append(d)
+            filter1.append(d)
         elif ticket_class == "1A" and val[-3] == 1:
-            ans.append(d)
+            filter1.append(d)
         elif ticket_class == None:
-            ans.append(d)
-
-    return ans
-print(search_trains('BCT', 'ADI',ticket_class = "1A"))
+            filter1.append(d)
+    
+    if departure_time or arrival_time:
+        filter2 = []
+        for val in filter1:
+            for slot in departure_time:
+                if slot == "slot1" and slot1 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot2>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+                if slot == "slot2" and slot2 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot3>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+                if slot == "slot3" and slot3 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot4>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+                if slot == "slot4" and slot4 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot5>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+                if slot == "slot5" and slot5 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot1>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+            for slot in arrival_time:
+                if slot == "slot1" and slot1 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot2>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+                if slot == "slot2" and slot2 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot3>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+                if slot == "slot3" and slot3 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot4>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+                if slot == "slot4" and slot4 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot5>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+                if slot == "slot5" and slot5 <= datetime.strptime(val["departure"], '%H:%M:%S') and slot1>= datetime.strptime(val["departure"], '%H:%M:%S'):
+                        filter2.append(val)
+        return filter2
+    return filter1
+print(search_trains('BCT', 'ADI',departure_time = ["slot1"]))
 def get_schedule(train_number):
     """Returns the schedule of a train.
     """
